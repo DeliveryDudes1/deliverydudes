1dd4461893307b54a7101b8116acde4ad76b0522
S00175857 <S00175857@mail.itsligo.ie>
2018-11-14 13:58:00 +0000

    nav,reg,login components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
